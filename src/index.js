09415d193485272c60c4db75549549338e252e8c
suryanshp1 <suryanshp1@gmail.com>
2024-07-20 15:15:27 +0530

    feat: added db connection client

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// require('dotenv').config({path: './env'})
+import dotenv from 'dotenv'
+import connectDB from './db/db.js';
+
+
+dotenv.config(
+    { 
+        path: './env' 
+    }
+);
+
+connectDB();
