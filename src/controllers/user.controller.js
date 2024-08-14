f35ed38ba95fdf021407cdd94734b0b50c884992
suryanshp1 <suryanshp1@gmail.com>
2024-08-15 01:54:01 +0530

    feat: added fix

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -220,7 +220,7 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
 
 
 const getCurrentUser = asyncHandler(async (req, res) => {
-    return res.status(200).json(200, req.user, "Current user fetched successfully")
+    return res.status(200).json(new ApiResponse(200, req.user, "Current user fetched successfully"))
 })
 
 const updateAccountDetails = asyncHandler(async (req, res) => {
@@ -260,6 +260,8 @@ const updateUserAvatar = asyncHandler(async (req, res) => {
             avatar: avatar.url
         }
     }, {new: true}).select("-password")
+
+    // TODO: delete old avatar from cloudinary
 
     return res.status(200).json(new ApiResponse(200, user, "Avatar image updated successfully"))
 })
